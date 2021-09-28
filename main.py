1761cfe5da7e9fa5f00757f7ec457cc5d7af0f44
NolanThrondset2022 <816059@mystma.org>
2021-09-28 14:32:13 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #WRITE YOUR CODE IN THIS FILE
+def function(password):
+    if password == "Knights19":
+        return "ACCESS GRANTED"
+    else:
+        return "ACCESS DENIED"
+print(function("Knights19"))
